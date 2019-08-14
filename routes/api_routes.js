08d37bd66ca3fb815dd844bf680b7b4e888055ea
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-14 14:33:52 -0400

    added user and post routes which link to api_routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const api_router = express().router()
+const api_routes = require('express').Router()
+const user_routes = require('./user_routes')
+const post_routes = require('./post_routes')
 
-api_router.get('/users',)
-api_router.get('/posts',)
+api_routes.get('/', (req, res) => {
+    res.send("API HOMEPAGE")
+})
+
+api_routes.use('/users', user_routes)
+api_routes.use('/posts', post_routes)
+
+module.exports = api_routes
