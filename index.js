b9760c0a79edee764b6e9187b6d65f6f69369687
Ajani Stewart <theamjaha@gmail.com>
2019-08-15 19:12:41 -0400

    fixes cors issue

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT || 5000
 const api_routes = require('./routes/api_routes')
 const seedDatabase = require('./seed_data')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 db.sync({force: true}).then(async () => {
     seedDatabase()
 
     app.use(bodyParser.json())
+    app.use(cors())
 
     // app.get('/', (req, res) => {
     //     res.send("THIS IS THE HOMEPAGE")
