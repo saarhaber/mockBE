95b9fb5df63c24ccb250b56572fbe45436757011
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-15 20:23:16 -0400

    master

--- database/models/interviews.js
+++ database/models/interviews.js
@@ -2,14 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Interviews = db.define("Interviews", {
-  // interviewerId: {
-  //   type: Sequelize.INTEGER,
-  //   allowNull: false
-  // },
-  // studentId: {
-  //   type: Sequelize.INTEGER,
-  //   allowNull: false
-  // },
   dateCreated: {
     type: Sequelize.DATE
   },
