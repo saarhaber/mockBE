cb087292ffd0ac117332b1e8874e93ba28ce05e0
Mahdiur Rahman <rmahdiur@gmail.com>
2019-08-21 13:51:42 -0400

    none

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const configureApp = () => {
         // });
         app.use(bodyParser.json())
         app.use(bodyParser.urlencoded({extended: true}))
-        app.use(cors({origin:'http://localhost:3001',credentials: true}));
+        app.use(cors({origin:'http://localhost:3000',credentials: true}));
     app.use(morgan('dev'))
 
     app.use(
